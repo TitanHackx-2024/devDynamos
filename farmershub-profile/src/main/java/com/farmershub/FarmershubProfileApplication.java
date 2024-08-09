87a1cb386a553557ed364988986775330d39e5c0
Ashwin <ashwinaradhya@gmail.com>
2024-08-09 19:16:36 +0530

    InitialProjectTemplate
    Spring boot template project for farmerprofile creation checkin.

---
+++ farmershub-profile/src/main/java/com/farmershub/FarmershubProfileApplication.java
@@ -0,0 +1,13 @@
+package com.farmershub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmershubProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmershubProfileApplication.class, args);
+	}
+
+}
---
+++ farmershub-profile/src/main/java/com/farmershub/ProfileController.java
@@ -0,0 +1,5 @@
+package com.farmershub;
+
+public class ProfileController {
+
+}
