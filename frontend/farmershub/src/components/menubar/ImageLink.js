a0aa4a14be15c233d24a2227e57e572eb5e29667
Mohmad Sufiyan <sufiyan.m@surya-soft.com>
2024-08-11 01:24:12 +0530

    👆 Created Clickable Img Link

---
+++ frontend/farmershub/src/components/menubar/ImageLink.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Image } from '@fluentui/react';
+import { useNavigate } from 'react-router-dom';
+
+const ImageLink = ({ src, alt, width, height }) => {
+  const nevigate = useNavigate();
+
+  const handleClick = () => {
+    nevigate('/Home');
+  };
+
+  return (
+    <div
+      onClick={handleClick}
+      style={{ cursor: 'pointer', marginBottom: '10px' }}
+    >
+      <Image
+        src={src}
+        alt={alt}
+        width={width}
+        height={height}
+      />
+    </div>
+  );
+};
+
+export default ImageLink;
