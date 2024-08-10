9ed3abffae845c9dd0825a5451ae523f64322d16
Mohmad Sufiyan <sufiyan.m@surya-soft.com>
2024-08-11 01:22:52 +0530

    🗺️ Added a Route

--- frontend/farmershub/src/Routes.js
+++ frontend/farmershub/src/Routes.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import SignUp from "./components/Signup";
 import Home from "./components/Home";
+import UserHome from './components/UserHome'
 import ProtectedRoute from "./components/ProtectedRoutes";
 
 function AppRoutes() {
@@ -10,14 +11,10 @@ function AppRoutes() {
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <Home />
-          </ProtectedRoute>
-        }
-      />
+      <Route element = {<ProtectedRoute/>}>
+          <Route path="/" element={<Home />}/>
+          <Route path="/home" element={<UserHome />}/>
+      </Route>
     </Routes>
   );
 }
