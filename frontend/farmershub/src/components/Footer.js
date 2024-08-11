3c70784b27305c1eaae2c713e9346ff008c84b63
Dhananjay <dhananjayjadhav2151@gmail.com>
2024-08-11 14:39:39 +0530

    searching added

--- frontend/farmershub/src/components/Footer.js
+++ frontend/farmershub/src/components/Footer.js
@@ -1,7 +1,22 @@
 function Footer() {
   return (
-    <footer className="container mx-auto flex justify-between items-center bg-slate-100">
-      This is footer
+    <footer className="container mx-auto flex justify-between items-center bg-slate-600 p-4">
+      <p className="text-white">
+        Made with <p className="inline text-red-500">♥</p> by Dev Dynamos for
+        TitanHackx 2024
+      </p>
+      <div className="flex space-x-4">
+        <a
+          href="https://github.com/TitanHackx-2024/devDynamos"
+          className="text-white hover:text-gray-300"
+        >
+          GitHub
+        </a>
+      </div>
+      <p className="text-white text-sm">
+        &copy; {new Date().getFullYear()} Tech Titan Community. All rights
+        reserved.
+      </p>
     </footer>
   );
 }
--- frontend/farmershub/src/constants/Constants.js
+++ frontend/farmershub/src/constants/Constants.js
@@ -2,3 +2,10 @@ export const signUpUrl = "http://localhost:8080/profile/create";
 export const loginUrl = "http://localhost:8080/profile/authenticate";
 export const fetchProfileByPincodeUrl =
   "http://localhost:8080/profile/pincode/";
+
+export const fetchProfileByFirstName =
+  "http://localhost:8080/profile/firstname/";
+
+export const fetchProfileByDistrict = "http://localhost:8080/profile/district/";
+
+export const fetchProfileByState = "http://localhost:8080/profile/state/";
