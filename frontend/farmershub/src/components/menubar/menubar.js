798de1768e6d1e09b06cb3869d13836d2899aa90
Mohmad Sufiyan <sufiyan.m@surya-soft.com>
2024-08-11 18:56:22 +0530

    ➕ Added Explore Product and Filter Link Btn In MenuBar

--- frontend/farmershub/src/components/menubar/menubar.js
+++ frontend/farmershub/src/components/menubar/menubar.js
@@ -4,42 +4,70 @@ import ColorBrand from './UserDetails/Avatar';
 import corplogoPath from '../../img/crop.png';
 import namelogoPath from '../../img/name.png';
 import SearchBar from './SearchBox';
-import ImageLink from './ImageLink'
+import ImageLink from './ImageLink';
+import IconLinkButton from '../IconLinkButton';
 
 const MenuBar = () => {
   return (
+    <Stack 
+      horizontal 
+      verticalAlign="center" 
+      tokens={{ padding: '10px' }} 
+      styles={{ root: { borderBottom: '1px solid #ccc' } }}
+    >
+      {/* Logo */}
+      <Stack.Item styles={{ root: { flexGrow: 1 } }}>
+        <Stack horizontal tokens={{ childrenGap: 10 }} verticalAlign="center">
+          <ImageLink
+            src={corplogoPath}
+            alt="FarmHub Logo"
+            width={40}
+            height={40}
+          />
+          <ImageLink
+            src={namelogoPath}
+            alt="FarmHub Logo Text"
+            width={140}
+            height={25}  // Increased height for the text logo
+          />
+        </Stack>
+      </Stack.Item>
+      {/* Search Bar */}
+      <Stack.Item styles={{ root: { flexGrow: 2 } }}>
+        <SearchBar placeholder="Search..." />
+      </Stack.Item>
+      {/* Icon Link Buttons with Descriptions */}
+      <Stack.Item styles={{ root: { flexGrow: 1 } }}>
         <Stack 
           horizontal 
-          verticalAlign="center" 
-          tokens={{ padding: '10px' }} 
-          styles={{ root: { borderBottom: '1px solid #ccc' } }}
+          tokens={{ childrenGap: 0 }} 
+          verticalAlign="center"
         >
-          {/* Logo */}
-          <Stack.Item styles={{ root: { flexGrow: 1 } }}>
-              <Stack horizontal tokens={{ childrenGap: 10 }} verticalAlign="center">
-              <ImageLink
-                src={corplogoPath}
-                alt="FarmHub Logo"
-                width={60}
-                height={40}
-              />
-              <ImageLink
-                src={namelogoPath}
-                alt="FarmHub Logo Text"
-                width={200}
-                height={35}
-              />
-            </Stack>
-          </Stack.Item>
-          {/* Search Bar */}
-          <Stack.Item styles={{ root: { flexGrow: 2 } }}>
-            <SearchBar placeholder="Search..." />
-          </Stack.Item>
-          {/* ColorBrand Component */}
-          <Stack.Item>
-            <ColorBrand init ='MS'/>
-          </Stack.Item>
+          <IconLinkButton
+            iconName="CartFilled"
+            ariaLabel="Products"
+            url="/products"
+            text="Products"
+          />
+          <IconLinkButton
+            iconName="FilterAddRegular"
+            ariaLabel="Filter"
+            url="/filter"
+            text="Filter"
+          />
+          <IconLinkButton
+            iconName="ArrowCircleUpRightFilled"
+            ariaLabel="Explore"
+            url="/explore"
+            text="Explore"
+          />
         </Stack>
+      </Stack.Item>
+      {/* ColorBrand Component */}
+      <Stack.Item >
+        <ColorBrand init='MS' />
+      </Stack.Item>
+    </Stack>
   );
 };
 
