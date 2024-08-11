76c3ce8bd53105adda9a6cb4dee9b81ca6f297da
Mohmad Sufiyan <sufiyan.m@surya-soft.com>
2024-08-11 18:14:13 +0530

    ➕ Added NotFound Page

--- frontend/farmershub/src/Routes.js
+++ frontend/farmershub/src/Routes.js
@@ -4,6 +4,7 @@ import Login from "./components/Login";
 import SignUp from "./components/Signup";
 import Home from "./components/Home";
 import UserHome from './components/UserHome'
+import NotFound from './components/NotFound'
 import ProtectedRoute from "./components/ProtectedRoutes";
 import Product from "./components/Products";
 
@@ -12,6 +13,7 @@ function AppRoutes() {
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="*" element={<NotFound />} />
       <Route element = {<ProtectedRoute/>}>
           <Route path="/" element={<Home />}/>
           <Route path="/home" element={<UserHome />}/>
